fa717fbea3c2d5e3c1705593243e4c071ceb000d
PCY00 <46348321+PCY00@users.noreply.github.com>
2024-10-24 15:31:31 +0900

    Update DualMode.py

--- src/Test/DoubleControl_DualMode/DualMode.py
+++ src/Test/DoubleControl_DualMode/DualMode.py
@@ -39,8 +39,13 @@ if MY_DXL == 'X_SERIES' or MY_DXL == 'MX_SERIES':
     
     DXL_MINIMUM_POSITION_VALUE  = 0         # Refer to the Minimum Position Limit of product eManual
     DXL_MAXIMUM_POSITION_VALUE  = 500       # Refer to the Maximum Position Limit of product eManual
-    DXL_REVERSE_MODE_VALUE = 0x01           # 시계방향(정방향)
-    DXL_REVERSE_SLAVE_MODE_VALUE = 0x03     # 시계방향(정방향) & slave 설정
+    DXL_2_MINIMUM_POSITION_VALUE = 0        # Refer to the Minimum Position Limit of product eManual
+    DXL_2_MAXIMUM_POSITION_VALUE  = 1800    # Refer to the Maximum Position Limit of product eManual
+    
+    
+    DXL_REVERSE_MODE_VALUE = 0x01           # 시계방향(시계 방향)
+    DXL_NORMAL_MODE_VALUE = 0x00            # 시계방향(반시계 방향) - 디폴트
+    DXL_REVERSE_SLAVE_MODE_VALUE = 0x02     # slave 설정
     BAUDRATE                    = 57600
 
 
@@ -66,7 +71,8 @@ PROFILE_ACCELERATION = 50  # 가속도 (rev/min^2)
 
 
 index = 0
-dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]         # Goal position
+dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]         # DXL_1_Goal position
+dxl_2_goal_position = [DXL_2_MINIMUM_POSITION_VALUE, DXL_2_MAXIMUM_POSITION_VALUE]   # DXL_2_Goal position
 
 # Initialize PortHandler instance
 # Set the port path
@@ -119,21 +125,25 @@ def set_profile_velocity_acceleration(velocity, acceleration, dxl_id):
     elif dxl_error != 0:
         print(f"Error: {packetHandler.getRxPacketError(dxl_error)}")
         
+        
+# Operating_Mode
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL1_ID, ADDR_OPERATING_MODE, 3)
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL2_ID, ADDR_OPERATING_MODE, 3)
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL3_ID, ADDR_OPERATING_MODE, 3)
+
 # 드라이브 모드 (회전방향을 시계방향으로 설정)
 dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL1_ID, ADDR_DRIVE_MODE, DXL_REVERSE_MODE_VALUE)
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL1_ID, ADDR_OPERATING_MODE, 3)
-
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL2_ID, ADDR_DRIVE_MODE, DXL_REVERSE_MODE_VALUE)
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL2_ID, ADDR_OPERATING_MODE, 3)
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL2_ID, ADDR_DRIVE_MODE, DXL_NORMAL_MODE_VALUE)
 
 # 드라이브 모드 (master-slave 설정)
 dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL3_ID, ADDR_DRIVE_MODE, DXL_REVERSE_SLAVE_MODE_VALUE)
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL3_ID, ADDR_OPERATING_MODE, 3)
 
 
 # Set the velocity and acceleration
 set_profile_velocity_acceleration(PROFILE_VELOCITY, PROFILE_ACCELERATION, DXL1_ID)
 set_profile_velocity_acceleration(PROFILE_VELOCITY, PROFILE_ACCELERATION, DXL2_ID)
+
+        
 
 # Enable Dynamixel#1 Torque
 dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL1_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
@@ -174,6 +184,8 @@ while 1:
     # Allocate goal position value into byte array
     param_goal_position = [DXL_LOBYTE(DXL_LOWORD(dxl_goal_position[index])), DXL_HIBYTE(DXL_LOWORD(dxl_goal_position[index])), DXL_LOBYTE(DXL_HIWORD(dxl_goal_position[index])), DXL_HIBYTE(DXL_HIWORD(dxl_goal_position[index]))]
 
+    param_goal_position2 = [DXL_LOBYTE(DXL_LOWORD(dxl_2_goal_position[index])), DXL_HIBYTE(DXL_LOWORD(dxl_2_goal_position[index])), DXL_LOBYTE(DXL_HIWORD(dxl_2_goal_position[index])), DXL_HIBYTE(DXL_HIWORD(dxl_2_goal_position[index]))]
+    
     # Add Dynamixel#1 goal position value to the Syncwrite parameter storage
     dxl_addparam_result = groupSyncWrite.addParam(DXL1_ID, param_goal_position)
     if dxl_addparam_result != True:
@@ -181,7 +193,7 @@ while 1:
         quit()
 
     # Add Dynamixel#2 goal position value to the Syncwrite parameter storage
-    dxl_addparam_result = groupSyncWrite.addParam(DXL2_ID, param_goal_position)
+    dxl_addparam_result = groupSyncWrite.addParam(DXL2_ID, param_goal_position2)
     if dxl_addparam_result != True:
         print("[ID:%03d] groupSyncWrite addparam failed" % DXL2_ID)
         quit()
@@ -219,9 +231,9 @@ while 1:
         dxl2_present_position = groupSyncRead.getData(DXL2_ID, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
        
 
-        print("[ID:%03d] GoalPos:%03d  PresPos:%03d\t[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL1_ID, dxl_goal_position[index], dxl1_present_position, DXL2_ID, dxl_goal_position[index], dxl2_present_position))
+        print("[ID:%03d] GoalPos:%03d  PresPos:%03d\t[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL1_ID, dxl_goal_position[index], dxl1_present_position, DXL2_ID, dxl_2_goal_position[index], dxl2_present_position))
 
-        if not ((abs(dxl_goal_position[index] - dxl1_present_position) > DXL_MOVING_STATUS_THRESHOLD) and (abs(dxl_goal_position[index] - dxl2_present_position) > DXL_MOVING_STATUS_THRESHOLD)):
+        if not ((abs(dxl_goal_position[index] - dxl1_present_position) > DXL_MOVING_STATUS_THRESHOLD) or (abs(dxl_2_goal_position[index] - dxl2_present_position) > DXL_MOVING_STATUS_THRESHOLD)):
             break
 
     # Change goal position
